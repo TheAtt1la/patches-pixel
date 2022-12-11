06af16b4b25e8fd7c72b3b0bafbbf827e1d61701
TheAtt1la <alittauwu@gmail.com>
2022-12-11 19:31:09 -0300

    add

---
+++ pixel.mk
@@ -0,0 +1,18 @@
+$(call inherit-product, vendor/aosp/config/common.mk)
+$(call inherit-product, vendor/aosp/config/BoardConfigSoong.mk)
+$(call inherit-product, device/aosp/sepolicy/common/sepolicy.mk)
+-include vendor/custom/build/core/config.mk
+
+TARGET_NO_KERNEL_OVERRIDE := true
+TARGET_NO_KERNEL_IMAGE := true
+SELINUX_IGNORE_NEVERALLOWS := true
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY := true
+TARGET_HAS_FUSEBLK_SEPOLICY_ON_VENDOR := true
+
+TARGET_FACE_UNLOCK_SUPPORTED := false
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_PACKAGES += \
+    libaptX_encoder \
+    libaptXHD_encoder
+
