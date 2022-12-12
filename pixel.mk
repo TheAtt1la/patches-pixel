384f714673268b55b76604b51928f1d08117a529
Alita <116470315+TheAtt1la@users.noreply.github.com>
2022-12-11 21:01:31 -0500

    Update pixel.mk

--- pixel.mk
+++ pixel.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, vendor/aosp/config/common.mk)
 $(call inherit-product, vendor/aosp/config/BoardConfigSoong.mk)
-$(call inherit-product, device/aosp/sepolicy/common/sepolicy.mk)
--include vendor/custom/build/core/config.mk
+$(call inherit-product, device/custom/sepolicy/common/sepolicy.mk)
+-include vendor/aosp/build/core/config.mk
 
 TARGET_NO_KERNEL_OVERRIDE := true
 TARGET_NO_KERNEL_IMAGE := true
@@ -9,7 +9,7 @@ SELINUX_IGNORE_NEVERALLOWS := true
 TARGET_USES_PREBUILT_VENDOR_SEPOLICY := true
 TARGET_HAS_FUSEBLK_SEPOLICY_ON_VENDOR := true
 
-TARGET_FACE_UNLOCK_SUPPORTED := false
+TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_BOOT_ANIMATION_RES := 1080
 
 PRODUCT_PACKAGES += \
